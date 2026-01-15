00f7cdd6a848743e72aa2fa6ad2d3ab3d3f37412
Olloberdi <olloberdinabiyev95@gmail.com>
2026-01-15 20:13:06 +0500

    birdan beshgacha bolgan misollar ishlandi

---
+++ main.py
@@ -0,0 +1,25 @@
+# 1
+# sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# kop = map(lambda value:value * 3,sonlar)
+# print(list(kop))
+
+# 2
+# sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# kva = list(map(lambda value: value ** 2,sonlar))
+# print(kva)
+
+# 3
+# matn = ['a','b','c','d']
+# upp = map(str.upper,matn)
+# print(list(upp))
+
+# 4
+# names = ['Toxir','Ali','Vali','Bakir']
+# get_len = map(len,names)
+# print(list(get_len))
+
+# 5
+# sonlar = [ -3, -6, -7, -10]
+# musbat = map(lambda value:value - 2*(value),sonlar)
+# print(list(musbat))
+
